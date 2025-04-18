55bbe372777075405e6eef571e5f455e2ad7dfea
Yeto4president <149055609+Yeto4president@users.noreply.github.com>
2025-04-18 11:34:38 +0200

    Update test_models.py

--- test_models.py
+++ test_models.py
@@ -1,8 +1,1 @@
-from src.modeling import get_models
-from sklearn.datasets import load_iris
 
-def test_models_initialization():
-    models = get_models()
-    iris = load_iris()
-    assert len(models) == 4
-    assert all(isinstance(model, type(models['KNN'])) for model in models.values())
